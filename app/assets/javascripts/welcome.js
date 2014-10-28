4fb4ec44977575874e9cf95fb5fe573cce1b63c7
Pete Brooks <peterossbrooks@gmail.com>
2014-10-28 16:41:02 -0500

    added tx

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -45,6 +45,17 @@ function ready() {
     var t = r.text() + " really";
     r.text(t);
   }, 800);
+
+  var i_vals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI'];
+  var i_el = $('span#xi');
+  var i_count = 0;
+  var interval = setInterval(xCountUp, 400);
+  function xCountUp() {
+    i_el.text(i_vals[i_count]);
+    if(i_count >= i_vals.length) {
+      clearInterval(interval);
+    } else { i_count++ }
+  };
 };
 
 $(document).ready(ready);
