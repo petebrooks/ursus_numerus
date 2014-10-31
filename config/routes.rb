b5e42efa2ac7a027319c3682b1ee14b3082d683e
Pete Brooks <peterossbrooks@gmail.com>
2014-10-31 16:15:12 -0500

    new route + text

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/if_I_could_work_anywhere' => 'welcome#jv'
   get '/id_love_to_join_your_team' => 'welcome#tx'
   get '/you_do_amazing_work' => 'welcome#v'
+  get '/no_team_id_rather_be_part_of' => 'welcome#v'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
